de459376ba0df91c4df9e2e459ac82ae05a57dc0
Mame29 <72637907+Mame29@users.noreply.github.com>
2021-10-31 14:35:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="indodax",
-    version="1.3",
+    version="1.3.1",
     author="Mame29",
     author_email="evanr0240@gmail.com",
     description="paket ini untuk jual beli di indodax",
